78b97cc92cbeb94122cb24ec3b3e73b7be7a6123
Jai-Prakash278 <jay065117@gmail.com>
2025-01-12 21:47:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function showSidebar(){
+    const sidebar = document.querySelector('#menulist,#apear')
+    menulist.style.display = 'flex'
+    apear.style.display = 'block'
+}
+function hideSidebar(){
+    const sidebar = document.querySelector('#menulist, #apear')
+    menulist.style.display = 'none'
+    apear.style.display = 'none'
+}
